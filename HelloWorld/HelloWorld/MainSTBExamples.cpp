e91d5eb7be13d59df54a8066b3624581dced220a
sugatmankar <sugatmankar@gmail.com>
2018-10-21 16:50:42 +0530

    vec3 class

---
+++ HelloWorld/HelloWorld/MainSTBExamples.cpp
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <iostream>
+
+#ifndef STBI_MSC_SECURE_CRT
+#define STBI_MSC_SECURE_CRT
+#endif // !STBI_MSC_SECURE_CRT
+
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#endif // !STB_IMAGE_IMPLEMENTATION
+
+
+#include "stb_image.h"
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif // !STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "stb_image_write.h"
+
+int mainstb() {
+
+	void HelloPPM(const char* fileName,int nx,int ny);
+	void HelloPPMCheckerBoard(const char* fileName, int nx, int ny);
+
+	void HelloBMPCheckerBoard(const char* fileName, int nx, int ny);
+
+	void HelloSTBPNGCheckerBoard1(const char* fileName, int nx, int ny);
+
+	int nx = 64;
+	int ny = 64;
+	
+	//using naked code
+	HelloPPM("Hello.ppm",nx,ny);
+	HelloPPMCheckerBoard("checker.ppm", nx, ny);
+	HelloBMPCheckerBoard("checker.bmp", nx, ny);
+	//HelloBMPCheckerBoard("checker.bmp", nx, ny);
+	//using stb image write
+	int x,y,n;
+	unsigned char* data = stbi_load("checker.bmp",&x,&y,&n,0);
+	stbi_write_png("foo_out.png",x,y,3,data,0);
+	
+	HelloSTBPNGCheckerBoard1("bar_out.png", 64, 64);
+
+	return 0;
+}
+
+void HelloSTBPNGCheckerBoard1(const char* fileName, int nx, int ny) {
+	
+	unsigned char* data = NULL;
+
+	data = (unsigned char*)malloc(3 * nx * ny); // 3 for x y z
+
+	memset(data,//dst
+			0 , //val
+			3 * nx * ny //size_t
+	);
+
+	int c = 0;
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+
+			c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 255;
+
+			data[(i + j * nx) * 3 + 0] = (unsigned char)(c);
+			data[(i + j * nx) * 3 + 1] = (unsigned char)(c);
+			data[(i + j * nx) * 3 + 2] = (unsigned char)(c);
+		}
+	}
+
+	stbi_write_png(fileName, nx, ny, 3, data, 0);
+
+	free(data);
+}
+
+void HelloPPM(const char* fileName,int nx,int ny) {
+	
+	FILE *gpFile = NULL;
+	if (fopen_s(&gpFile, fileName, "w")) {
+		std::cout << "File can't created \n" << std::endl;
+		exit(0);
+	}
+	else {
+		std::cout << "File created successfully " << std::endl;
+	}
+
+	fprintf(gpFile, "P3\n%d %d\n255\n", nx, ny);
+
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2f;
+			int ir = int(255.99f * r);
+			int ig = int(255.99f * g);
+			int ib = int(255.99f * b);
+			fprintf(gpFile, "%d %d %d\n", ir, ig, ib);
+		}
+	}
+
+	if (gpFile) {
+		std::cout << "File closed successfully" << std::endl;
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
+
+void HelloPPMCheckerBoard(const char* fileName, int nx, int ny) {
+	FILE *gpFile = NULL;
+	if (fopen_s(&gpFile, fileName, "w")) {
+		std::cout << "File can't created \n" << std::endl;
+		exit(0);
+	}
+	else {
+		std::cout << "File created successfully " << std::endl;
+	}
+
+	fprintf(gpFile, "P3\n%d %d\n255\n", nx, ny);
+	int c = 0;
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 255;
+			fprintf(gpFile, "%d %d %d\n", c, c, c);
+		}
+	}
+
+	if (gpFile) {
+		std::cout << "File closed successfully" << std::endl;
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+
+}
+
+void HelloBMPCheckerBoard(const char* fileName, int nx, int ny) {
+
+	//Referance : https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
+
+	FILE *gpFile = NULL;
+	int w = nx;
+	int h = ny;
+	//int x, y = 0;
+	unsigned char* img = NULL;
+
+	int filesize = 54 + 3 * w * h; //54 BMP header size on windows, 3 is for r g b aand w and h as per requirement
+
+	img = (unsigned char*)malloc(3 * w * h);
+	memset(img, 0, 3 * w * h);
+
+	/*int c = 0;
+	for (int i = 0; i < w; i++) {
+		for (int j = 0; j < h; j++) {
+			x = i;
+			y = (h - 1) - j;
+			c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 255;
+
+			img[(x + y * w) * 3 + 2] = (unsigned char)(c);
+			img[(x + y * w) * 3 + 1] = (unsigned char)(c);
+			img[(x + y * w) * 3 + 0] = (unsigned char)(c);
+		}
+	}*/
+
+	int c = 0;
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+
+			c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 255;
+			
+			img[(i + j * w) * 3 + 0] = (unsigned char)(c);
+			img[(i + j * w) * 3 + 1] = (unsigned char)(c);
+			img[(i + j * w) * 3 + 2] = (unsigned char)(c);
+			//img[(i + j * w) * 3 + 3] = (unsigned char)(255);
+		}
+	}
+
+	unsigned char bmpfileheader[14] = { 'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0 };
+	unsigned char bmpinfoheader[40] = { 40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0 };
+	unsigned char bmppad[3] = { 0,0,0 };
+
+	bmpfileheader[2] = (unsigned char)(filesize);
+	bmpfileheader[3] = (unsigned char)(filesize >> 8);
+	bmpfileheader[4] = (unsigned char)(filesize >> 16);
+	bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+	bmpinfoheader[4] = (unsigned char)(w);
+	bmpinfoheader[5] = (unsigned char)(w >> 8);
+	bmpinfoheader[6] = (unsigned char)(w >> 16);
+	bmpinfoheader[7] = (unsigned char)(w >> 24);
+	bmpinfoheader[8] = (unsigned char)(h);
+	bmpinfoheader[9] = (unsigned char)(h >> 8);
+	bmpinfoheader[10] = (unsigned char)(h >> 16);
+	bmpinfoheader[11] = (unsigned char)(h >> 24);
+
+	if (fopen_s(&gpFile, fileName, "wb")) {
+		std::cout << "File can't created \n" << std::endl;
+		exit(0);
+	}
+	else {
+		std::cout << "File created successfully " << std::endl;
+	}
+
+	fwrite(bmpfileheader, 1, 14, gpFile);
+	fwrite(bmpinfoheader, 1, 40, gpFile);
+	for (int i = 0; i<h; i++)
+	{
+	
+		fwrite(img + (w*(h - i - 1) * 3), 3, w, gpFile);
+		fwrite(bmppad, 1, (4 - (w * 3) % 4) % 4, gpFile);
+	}
+
+	free(img);
+
+	if (gpFile) {
+		std::cout << "File closed successfully" << std::endl;
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+
+}
--- HelloWorld/HelloWorld/vec3.h
+++ HelloWorld/HelloWorld/vec3.h
@@ -1,52 +1,75 @@
-#include<iostream>
-#include<stdio.h>
-#include<math.h>
+#pragma once
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
 
-class vec3 {
+class vec3
+{
 public:
-
 	float e[3];
 
-	vec3() {}
-
+	vec3() {
+	}
+		
 	vec3(float e0, float e1, float e2) {
 		e[0] = e0;
 		e[1] = e1;
 		e[2] = e2;
 	}
+	
+	/* C++ inline function is powerful concept that is commonly used with classes.
+	If a function is inline, the compiler places a copy of the code of that function 
+	at each point where the function is called at compile time. ... 
+	The compiler can ignore the inline qualifier in case defined function is more than a line. */
 
+	/*
+	A "const function", denoted with the keyword const after a function declaration, 
+	makes it a compiler error for this class function to change a member variable of the class. 
+	However, reading of a class variables is ok inside of the function, 
+	but writing inside of this function will generate a compiler error.
+
+	Another way of thinking about such "const function" is by viewing an class function as a 
+	normal function taking an implicit this pointer. So a method 
+	int Foo::Bar(int random_arg) 
+	(without the const at the end) results in a function like 
+	int Foo_Bar(Foo* this, int random_arg), 
+	and a call such as 
+	Foo f; f.Bar(4)
+	will internally correspond to something like
+	Foo f; Foo_Bar(&f, 4).
+	Now adding the const at the end 
+	(int Foo::Bar(int random_arg) const) 
+	can then be understood as a declaration with a const this pointer: 
+	int Foo_Bar(const Foo* this, int random_arg). 
+	Since the type of this in such case is const, 
+	no modifications of member variables are possible.
+	*/
 	inline float x() const {
 		return e[0];
 	}
-
 	inline float y() const {
 		return e[1];
 	}
-
 	inline float z() const {
 		return e[2];
 	}
-
 	inline float r() const {
 		return e[0];
 	}
-
 	inline float g() const {
 		return e[1];
 	}
-
 	inline float b() const {
 		return e[2];
 	}
 
-	inline const vec3& operator+() const {
+	inline const vec3& operator+()const {
 		return *this;
 	}
 
 	inline vec3 operator-() const {
-		return vec3(-e[0], -e[1], -e[3]);
+		return vec3(-e[0],-e[1],-e[2]);
 	}
-
 	inline float operator[](int i) const {
 		return e[i];
 	}
@@ -59,117 +82,21 @@ public:
 	inline vec3& operator-=(const vec3 &v2);
 	inline vec3& operator*=(const vec3 &v2);
 	inline vec3& operator/=(const vec3 &v2);
+
 	inline vec3& operator*=(const float t);
 	inline vec3& operator/=(const float t);
 
 	inline float length() const {
-		return sqrt(e[0] * e[0] + e[1] * e[1] + e[1] * e[2]);
+		return sqrt(e[0]*e[0] + e[1] * e[1] + e[2] * e[2]);
 	}
 
-	inline float squared_length() const {
-		return (e[0] * e[0] + e[1] * e[1] + e[1] * e[2]);
+	inline float squared_length()const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
 
 	inline void make_unit_vector();
+
+	~vec3() {
+	}
 };
-
-inline std::istream& operator>>(std::istream &is, vec3 &t) {
-	is >> t.e[0] >> t.e[1] >> t.e[2];
-	return is;
-}
-
-inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
-	os << t.e[0] << t.e[1] << t.e[2];
-	return os;
-}
-
-inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
-	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
-}
-
-inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
-	return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
-}
-
-inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
-	return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
-}
-
-inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
-	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
-}
-
-inline vec3 operator*(float t, const vec3 &v) {
-	return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
-}
-
-inline vec3 operator/(float t, const vec3 &v) {
-	return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
-}
-
-inline vec3 operator*(const vec3 &v, float t) {
-	return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
-}
-
-inline vec3 operator/(const vec3 &v, float t) {
-	return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
-}
-
-inline float dot(const vec3& v1, const vec3& v2) {
-	return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
-}
-
-inline vec3 cross(const vec3& v1, const vec3& v2) {
-	return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
-		(-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
-		(v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0])
-	);
-}
-
-inline vec3& vec3::operator+=(const vec3 &v) {
-	e[0] += v.e[0];
-	e[1] += v.e[1];
-	e[2] += v.e[2];
-	return(*this);
-}
-
-inline vec3& vec3::operator-=(const vec3 &v) {
-	e[0] -= v.e[0];
-	e[1] -= v.e[1];
-	e[2] -= v.e[2];
-	return(*this);
-}
-
-inline vec3& vec3::operator*=(const vec3 &v) {
-	e[0] *= v.e[0];
-	e[1] *= v.e[1];
-	e[2] *= v.e[2];
-	return(*this);
-}
-
-inline vec3& vec3::operator/=(const vec3 &v) {
-	e[0] /= v.e[0];
-	e[1] /= v.e[1];
-	e[2] /= v.e[2];
-	return(*this);
-}
-
-inline vec3& vec3::operator*=(const float t) {
-	e[0] *= t;
-	e[1] *= t;
-	e[2] *= t;
-	return(*this);
-}
-
-inline vec3& vec3::operator/=(const float t) {
-	float k = 1.0 / t;
-	e[0] *= k;
-	e[1] *= k;
-	e[2] *= k;
-	return(*this);
-}
-
-inline vec3 unit_vector(vec3 v) {
-	return v / v.length();
-}
 
